8aca6726da6e03de2558977d2047b273fd7d07cd
Byanew23 <borislav.yanev23@gmail.com>
2022-12-06 17:34:14 +0200

    Add solution for day6

--- 2022/day6/day6.py
+++ 2022/day6/day6.py
@@ -1,13 +1,27 @@
-fileName = "day6/sample.txt"
+fileName = "day6/input.txt"
 
-def solveFirst(file):
+def findUniqueSequence(file, length):
     file = open(file, 'r')
     line = file.readline()
-    diffLet = ""
-    index = 0
+    diffLet = line[0:length:]
+    res = 0
+    for i in range(length, len(line)):
+        pairs = []
+        for x in range(len(diffLet)):
+            if diffLet[x] not in pairs:
+                if(x == length-1):
+                    res = i
+                    break
+                pairs.append(diffLet[x])
+                continue
+            else:
+                diffLet = line[i-(length-1):i+1:]
+                break
+        if res >0:
+            break
 
-    for char in line:
-        if char not in diffLet:
-            diffLet += char
 
-solveFirst(fileName)
+    print('The solution is:', res)
+
+findUniqueSequence(fileName, 4)
+findUniqueSequence(fileName, 14)
